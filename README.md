664af43f2bb0891d414879fc0e27c115edec7257
iseyoshitaka <ise0615@gmail.com>
2021-05-22 18:40:07 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# vue-image-base64
+ 
+画像ファイルをBase64に変換するVue.js用コンポーネントです
+ 
+# DEMO
+ 
+https://isystk.github.io/vue-image-base64
+ 
+# Features
+ 
+- ファイルを選択するとBase64に変換されたデータをJSON形式で返却します。
+- 画像のサイズを指定することでリサイズすることが可能です。
+- HEIC形式の画像の場合はJPEGに変換します。
+- ドラッグ＆ドロップでアップロードファイルの選択が可能です。
+ 
+# Requirement
+ 
+* heic2any 0.0.3
+ 
+# Installation
+ 
+```bash
+yarn add -D vue-image-base64
+```
+ 
+# Usage
+ 
+```bash
+git clone https://github.com/isystk/vue-image-base64.git
+cd vue-image-base64
+yarn run start
+```
+ 
+# Note
+
+```
+  const [images, setImages] = useState({data: []});
+  const [errors, setErrors] = useState([]);
+
+  return (
+    <div>
+      <vueImageBase64
+        maxFileSize={10485760}
+        thumbnail_size={100}
+        drop={true}
+        dropText="ファイルをドラッグ＆ドロップもしくは"
+        capture="environment"
+        multiple={true}
+        handleChange={data => {
+          if (data.result) {
+            let list =images.data
+            list.push(data);
+            setImages({data: list})
+          } else {
+            setErrors([...errors, data.messages]);
+          }
+        }}
+      />
+      { errors.map((error, index) => 
+          <p className="error-message" key={index}>{error}</p>
+        )
+      }
+    </div>
+  )
+```
+ 
+# Author
+ 
+[isystk](https://github.com/isystk)
+
+# License
+ 
+[MIT](https://en.wikipedia.org/wiki/MIT_License).
---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="root">
+    <div class="wrap">
+      <header>
+        <div class="header-image">
+          <div class="logo">VUE IMAGE BASE64</div>
+        </div>
+      </header>
+      <div class="content">
+        <main>
+          <article class="detail">
+            <div class="entry-header">
+              <h1 class="entry-title">
+                <a href="#">画像ファイルをBase64に変換するVue.js用コンポーネントです</a>
+              </h1>
+            </div>
+            <div class="entry-content">
+              <h2>デモ</h2>
+              <VueImageBase64 
+                :maxFileSize="10485760"
+                :thumbnail_size="100"
+                :drop="true"
+                dropText="ファイルをドラッグ＆ドロップもしくは"
+                capture="environment"
+                :multiple="true"
+                @handleChange="handleChange"
+              />
+              <div>
+                <table id="select-image">
+                  <thead>
+                    <tr>
+                      <th>ファイル名</th>
+                      <th>元の画像</th>
+                      <th>リサイズ後画像</th>
+                      <th>元のサイズ</th>
+                      <th>リサイズ後サイズ</th>
+                      <th>タイプ</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </article>
+        </main>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import VueImageBase64 from './components/VueImageBase64.vue'
+
+export default {
+  name: 'App',
+  components: {
+    VueImageBase64
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+    handleChange: function(data) {
+      console.log(data);
+      // if (data.result) {
+      //   let list =images.data
+      //   list.push(data);
+      //   setImages({data: list})
+      // } else {
+      //   setErrors([...errors, data.messages]);
+      // }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
